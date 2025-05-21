eb91cb209713f48e242b7df080837257c753e29e
Rakkio <sbierhffor@gmail.com>
2025-05-21 17:36:45 +0200

    Add google-site-verifiaction

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="1WEn2r9gaNYZTMjSdKEnEnT3reuAdumNhy3NqH_ICaU" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
