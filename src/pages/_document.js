ad13d5034027bb11b34bc75e0ca4d7f7c97f7e02
Rakkio <sbierhffor@gmail.com>
2025-05-22 02:04:50 +0200

    add tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,18 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="1WEn2r9gaNYZTMjSdKEnEnT3reuAdumNhy3NqH_ICaU" />
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17102863562"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17102863562');
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
         <Main />
